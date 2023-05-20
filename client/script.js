a663864a1fd0272494726816cf4ac26a5bad453b
Mrunal Vaidya <91138133+Mrunal0803@users.noreply.github.com>
2023-05-20 10:18:45 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://kisan-gpt.onrender.com/', {
+    const response = await fetch('https://kisan-kblj.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
